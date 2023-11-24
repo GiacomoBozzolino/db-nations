95e9f1307b141ed50706c7c3cfb32d5395341d1a
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-11-24 15:43:40 +0100

    bomus completato

--- src/main/java/org/lessons/java/Main.java
+++ src/main/java/org/lessons/java/Main.java
@@ -31,7 +31,9 @@ public class Main {
 						 +" ; ";  
 		  
 		  try(PreparedStatement ps = con.prepareStatement(sql)){
+			  
 			  ps.setString(1, "%" + strName + "%");
+			  
 		    try(ResultSet rs = ps.executeQuery()){
 		    	
 		    	while(rs.next()) {
@@ -47,6 +49,52 @@ public class Main {
 		    	}
 		    }
 		  }
+		  
+		  System.out.print("Inserisci l'id di una country per ulteriori informazioni: ");
+          int selectedCountryId = in.nextInt();
+
+          // Query per ottenere tutte le lingue parlate in quella country
+          final String languagesSql = "SELECT languages.language "
+                  + "FROM countries "
+                  + "JOIN country_languages ON countries.country_id = country_languages.country_id "
+                  + "JOIN languages ON country_languages.language_id = languages.language_id "
+                  + "WHERE countries.country_id = ?";
+          try (PreparedStatement languagesPs = con.prepareStatement(languagesSql)) {
+              languagesPs.setInt(1, selectedCountryId);
+
+              try (ResultSet languagesRs = languagesPs.executeQuery()) {
+                  System.out.println("Lingue parlate nella country con ID " + selectedCountryId + ":");
+                  while (languagesRs.next()) {
+                      String language = languagesRs.getString(1);
+                      System.out.println(language);
+                  }
+              }
+          }
+
+          // Query per ottenere le statistiche più recenti per quella country
+          final String statsSql = "SELECT country_stats.year, country_stats.population, country_stats.gdp "
+                  + "FROM countries "
+                  + "JOIN country_stats ON countries.country_id = country_stats.country_id "
+                  + "WHERE countries.country_id = ? "
+                  + "ORDER BY country_stats.year DESC "
+                  + "LIMIT 1";
+          try (PreparedStatement statsPs = con.prepareStatement(statsSql)) {
+              statsPs.setInt(1, selectedCountryId);
+
+              try (ResultSet statsRs = statsPs.executeQuery()) {
+                  System.out.println("Ultime statistiche per la country con ID " + selectedCountryId + ":");
+                  while (statsRs.next()) {
+                      int year = statsRs.getInt("year");
+                      int population = statsRs.getInt("population");
+                      double gdp = statsRs.getDouble("gdp");
+
+                      System.out.println("Anno: " + year);
+                      System.out.println("Popolazione: " + population);
+                      System.out.println("GDP: " + gdp);
+                  }
+              }
+          }
+		  
 		} catch (Exception e) {
 			
 			System.out.println("Error in db: " + e.getMessage());
